82002bb8c27178e0ffff0986ab0322368eca8659
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-02-21 21:19:56 -0800

    refactor: use check function for test

--- src/test/java/net/peterv/bazillionaire/game/domain/ticker/strategy/CyclePricingStrategyTest.java
+++ src/test/java/net/peterv/bazillionaire/game/domain/ticker/strategy/CyclePricingStrategyTest.java
@@ -10,15 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CyclePricingStrategyTest {
 
-	@Test
-	void pricesStayWithinBoundedRange() {
-		var initialPrice = new Money(100_00);
-		int stepCents = 1_00;
-		int cycleDuration = 100;
-		var strategy = new CyclePricingStrategy(initialPrice, stepCents, cycleDuration, 1);
-
-		int minExpected = initialPrice.cents();
-		int maxExpected = initialPrice.cents() + (stepCents * cycleDuration);
+	private void check(
+			Money initialPrice,
+			int stepCents,
+			int cycleDuration,
+			int direction,
+			int minExpected,
+			int maxExpected
+	) {
+		var strategy = new CyclePricingStrategy(initialPrice, stepCents, cycleDuration, direction);
 
 		boolean allWithinRange = IntStream.range(0, cycleDuration)
 				.mapToObj(i -> strategy.nextPrice(MarketForce.neutral()))
@@ -31,22 +31,22 @@ class CyclePricingStrategyTest {
 	}
 
 	@Test
+	void pricesStayWithinBoundedRange() {
+		var initialPrice = new Money(100_00);
+		int stepCents = 1_00;
+		int cycleDuration = 100;
+		check(initialPrice, stepCents, cycleDuration, 1,
+				initialPrice.cents(),
+				initialPrice.cents() + stepCents * cycleDuration);
+	}
+
+	@Test
 	void pricesStayWithinBoundedRangeNegative() {
 		var initialPrice = new Money(100_00);
 		int stepCents = 1_00;
 		int cycleDuration = 100;
-		var strategy = new CyclePricingStrategy(initialPrice, stepCents, cycleDuration, -1);
-
-		int maxExpected = initialPrice.cents();
-		int minExpected = initialPrice.cents() - (stepCents * cycleDuration);
-
-		boolean allWithinRange = IntStream.range(0, cycleDuration)
-				.mapToObj(i -> strategy.nextPrice(MarketForce.neutral()))
-				.map(Money::cents)
-				.allMatch(p -> p <= maxExpected && p >= minExpected);
-
-		assertEquals(StrategyKind.CYCLE, strategy.kind());
-		assertTrue(strategy.isExhausted(), "Strategy is exhausted");
-		assertTrue(allWithinRange, "Prices escape range");
+		check(initialPrice, stepCents, cycleDuration, -1,
+				initialPrice.cents() - stepCents * cycleDuration,
+				initialPrice.cents());
 	}
 }
