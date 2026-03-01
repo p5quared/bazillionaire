9df54aef774b8abfef72f24e1fcbd0200e6462af
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-02-28 21:37:17 -0800

    feat: init UserController.java

---
+++ src/main/java/net/peterv/bazillionaire/web/UserController.java
@@ -0,0 +1,67 @@
+package net.peterv.bazillionaire.web;
+
+import io.quarkiverse.renarde.Controller;
+import io.quarkiverse.renarde.util.RedirectException;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import net.peterv.bazillionaire.user.models.User;
+import org.jboss.resteasy.reactive.RestForm;
+
+import java.net.URI;
+import java.util.List;
+
+@Path("/users")
+public class UserController extends Controller {
+
+	@Inject
+	UserService userService;
+
+	@CheckedTemplate
+	static class Templates {
+		public static native TemplateInstance list(List<User> users);
+
+		public static native TemplateInstance edit(User user);
+	}
+
+	@GET
+	@Path("/")
+	@Transactional
+	public TemplateInstance list() {
+		List<User> users = User.listAll();
+		return Templates.list(users);
+	}
+
+	@GET
+	@Path("/{id}")
+	@Transactional
+	public TemplateInstance edit(@PathParam("id") Long id) {
+		User user = User.findById(id);
+		if (user == null) {
+			flash("error", "User not found");
+			throw new RedirectException(Response.seeOther(URI.create("/users")).build());
+		}
+		return Templates.edit(user);
+	}
+
+	@POST
+	@Path("/{id}")
+	public void update(@PathParam("id") Long id, @RestForm String username) {
+		if (username == null || username.isBlank()) {
+			flash("error", "Username cannot be blank");
+			throw new RedirectException(Response.seeOther(URI.create("/users/" + id)).build());
+		}
+		userService.updateUser(id, username.strip());
+		throw new RedirectException(Response.seeOther(URI.create("/users")).build());
+	}
+
+	@POST
+	@Path("/{id}/delete")
+	public void delete(@PathParam("id") Long id) {
+		userService.deleteUser(id);
+		throw new RedirectException(Response.seeOther(URI.create("/users")).build());
+	}
+}
---
+++ src/main/java/net/peterv/bazillionaire/web/UserService.java
@@ -0,0 +1,25 @@
+package net.peterv.bazillionaire.web;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import net.peterv.bazillionaire.user.models.User;
+
+@ApplicationScoped
+public class UserService {
+
+	@Transactional
+	public void updateUser(Long id, String username) {
+		User user = User.findById(id);
+		if (user == null)
+			return;
+		user.username = username;
+	}
+
+	@Transactional
+	public void deleteUser(Long id) {
+		User user = User.findById(id);
+		if (user == null)
+			return;
+		user.delete();
+	}
+}
